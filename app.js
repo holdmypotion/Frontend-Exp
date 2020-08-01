d7bf6088f2994c1e02dc2ddfd234b4bf2829f68e
Elijah <rahulsharma240301@gmail.com>
2020-08-01 23:18:40 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const intro = document.querySelector(".intro");
+const video = intro.querySelector("video");
+const text = intro.querySelector("h1");
+
+// End section
+const section = document.querySelector("section");
+const end = section.querySelector("h1");
+
+// SCROLL MAGIC
+
+const controller = new ScrollMagic.Controller();
+
+//SCENES
+const scene = new ScrollMagic.Scene({
+  duration: 6000,
+  triggerElement: intro,
+  triggerHook: 0,
+})
+  .addIndicators()
+  .setPin(intro)
+  .addTo(controller);
+
+//VIDEO ANIMATION
+let accelAmount = 0.1;
+let scrollPos = 0;
+let delay = 0;
+
+scene.on("update", (e) => {
+  scrollPos = e.scrollPos / 1000;
+  // console.log(e);
+});
+
+setInterval(() => {
+  delay += (scrollPos - delay) * accelAmount;
+  console.log(scrollPos, delay);
+
+  // video.currentTime = scrollPos;
+  video.currentTime = delay;
+}, 50);
+// put 1000/frameRate here
